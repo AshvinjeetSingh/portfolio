8940623d38927734ed7af7032d7262bb0a266e8f
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2024-05-13 03:12:54 -0400

    design changes

--- src/components/Global/hamburger-menu.tsx
+++ src/components/Global/hamburger-menu.tsx
@@ -9,35 +9,32 @@ const HamburgerMenu = (props: Props) => {
     const [state, setState] = React.useState(false)
 
     const menus = [
-        { title: "Home", path: "/your-path" },
-        { title: "Blog", path: "/your-path" },
-        { title: "About Us", path: "/your-path" },
-        { title: "Contact Us", path: "/your-path" },
+        { title: "Home", path: "#home" },
+        { title: "About", path: "#about" },
+        { title: "Projects", path: "#projects" },
+        { title: "Contact", path: "#contact" },
     ]
 
   return (
-        <nav className="bg-white w-full border-b md:border-0 md:hidden">
+        <nav className=" w-full md:border-0 md:hidden relative">
           <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
               <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                  <Link href="/">
-                      <h1 className="text-3xl font-bold text-purple-600">Logo</h1>
-                  </Link>
                   <div className="md:hidden">
                       <button
                           className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
                           onClick={() => setState(!state)}
                       >
-                          <Menu />
+                          <Menu className='text-black dark:text-white'/>
                       </button>
                   </div>
               </div>
               <div
                   className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${state ? "block" : "hidden"
-                      }`}
+                      } absolute top-15 dark:bg-black bg-white text-black dark:text-white`}
               >
                   <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
                       {menus.map((item, idx) => (
-                          <li key={idx} className="text-gray-600 hover:text-indigo-600">
+                          <li key={idx} className="text-black dark:text-white hover:text-amber-600" onClick={() => setState(!state)}>
                               <Link href={item.path}>{item.title}</Link>
                           </li>
                       ))}
