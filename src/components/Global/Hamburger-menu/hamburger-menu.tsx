c4c09ce8d7ea089fd8256089d40bc3fa26ed4b3f
Ashvinjeet-singh <ashvinjeetsingh98@gmail.com>
2024-05-21 23:08:32 -0400

    hamburger footer

--- src/components/Global/Hamburger-menu/hamburger-menu.tsx
+++ src/components/Global/Hamburger-menu/hamburger-menu.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss'
 import { useEffect, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import { useParams, usePathname } from 'next/navigation';
+import { ArrowDown, ArrowDownToLine, ArrowDownToLineIcon, Github, LinkedinIcon, Twitter } from 'lucide-react';
 import { motion } from 'framer-motion';
 import { menuSlide } from './anim';
 import Link from "./Link/index"
@@ -37,32 +38,7 @@ const HamburgerMenu = (props: Props) => {
     const [isActive, setIsActive] = useState(false);
     const pathname = usePathname();
     const [selectedIndicator, setSelectedIndicator] = useState(pathname);
-    // useEffect(() => {
-    //     const section = hash.replace("#", "");
-    //     console.log("Hash",section)
-    //     if (section) scrollToSection(section);
-    //     if(isActive) setIsActive(false)
-    // }, [hash]);
 
-    // useEffect(() => {
-    //     // console.log("pathname",pathname)
-    //     console.log("pathname", window.location.hash)
-    //     if(isActive){
-    //         setIsActive(!isActive) 
-    //     }
-    //     const hash = (asPath as string).split("#")[1]; // error=unauthorized_client&error_code=401error_description=Something+went+wrong
-    //     const parsedHash = new URLSearchParams(hash);
-    //     const errorHash = parsedHash.get("error_description");
-    // }
-    // , [window.location.hash])
-
-    // useEffect(() => {
-    //     const onHashChange = () => {
-    //         setHash(window.location.hash);
-    //     };
-    //     window.addEventListener('hashchange', onHashChange);
-    //     return () => window.removeEventListener('hashchange', onHashChange);
-    // }, []);
 
 
 
@@ -71,33 +47,42 @@ const HamburgerMenu = (props: Props) => {
             <div className={`${styles.main} md:hidden`}>
 
                 <div className={styles.header}>
-                    <div onClick={() => { setIsActive(!isActive) }} className={`${styles.button} bg-black dark:bg-white` } >
+                    <div onClick={() => { setIsActive(!isActive) }} className={`${styles.button} bg-black dark:bg-white`} >
                         <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}  after:dark:bg-black after:bg-white  before:dark:bg-black before:bg-white`}></div>
                     </div>
                 </div>
 
             </div>
             <AnimatePresence mode="wait">
-                {isActive && 
-                <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={`${styles.menu} dark:bg-[#1c1c1c] bg-white text-black dark:text-white`}>
-                    <div className={styles.body}>
-                        <div onMouseLeave={() => { setSelectedIndicator(window.location.hash) }} className={styles.nav}>
-                            <div className={styles.header}>
-                                <p>Navigation</p>
+                {isActive &&
+                    <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={`${styles.menu} dark:bg-[#1c1c1c] bg-white text-black dark:text-white`}>
+                        <div className={styles.body}>
+                            <div onMouseLeave={() => { setSelectedIndicator(window.location.hash) }} className={styles.nav}>
+                                <div className={styles.header}>
+                                    <p>Navigation</p>
+                                </div>
+                                {
+                                    navItems.map((data, index) => {
+                                        return <Link key={index} data={{ ...data, index }} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator} setIsActive={setIsActive}></Link>
+                                    })
+                                }
                             </div>
-                            {
-                                navItems.map((data, index) => {
-                                    return <Link key={index} data={{ ...data, index }} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator} setIsActive={setIsActive}></Link>
-                                })
-                            }
+                            <div className={styles.header}>
+                                <ul className='flex justify-center item-center text-white dark:text-black'>
+                                    <li className="px-3 dark:text-white h-full text-black transition-all duration-500  "><a href="https://github.com/AshvinjeetSingh" target="_blank" rel="noopener noreferrer"><Github /></a></li>
+                                    <li className="px-3 dark:text-white text-black transition-all duration-500 h-full   "><a href="https://www.linkedin.com/in/ashvinjeetsingh" target="_blank" rel="noopener noreferrer"><LinkedinIcon /></a></li>
+                                    <li className="px-3 dark:text-white text-black transition-all duration-500  h-full "><a href="https://twitter.com/AshvinjeetS" target="_blank" rel="noopener noreferrer"><Twitter /></a></li>
+                                    <li className="px-3 dark:text-white text-black transition-all duration-500  h-full">
+                                        <a href="/CV.pdf" download="cv" ><ArrowDownToLineIcon /></a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        {/* <Footer /> */}
-                    </div>
-                    <Curve />
-                </motion.div>}
+                        <Curve />
+                    </motion.div>}
             </AnimatePresence>
         </>
-    ) 
+    )
 }
 
 export default HamburgerMenu
